e0551f00c5c0a22cd1a1f52cf85a12495e8381a3
Jervies Lachica <lachicajervies>
2026-09-26 12:00:15 +0800

    chore(release): bump version to 2.1.5 (code 16)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.edukasyon.studentai"
         minSdk = 24
         targetSdk = 34
-        versionCode = 15
-        versionName = "2.1.4"
+        versionCode = 16
+        versionName = "2.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
